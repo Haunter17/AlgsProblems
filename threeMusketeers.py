2f8581857dd74d68bad73d9e2cbe1f43878f7fbe
Haunter17 <haunterhacker17@gmail.com>
2016-10-14 00:08:38 -0700

    fixing cycles of three, but still having issues of cycles of k

--- threeMusketeers.py
+++ threeMusketeers.py
@@ -4,6 +4,10 @@
 #         'D': set(['B']),
 #         'E': set(['B', 'F']),
 #         'F': set(['C', 'E'])}
+graphB = {1: set([2,4]),
+          2: set([1,3]),
+		  3: set([2,4]),
+		  4: set([1,3])}
 import copy
 class Solution:
 	def generateGraph(self, path):
@@ -24,28 +28,31 @@ class Solution:
 		queue = [start]
 
 		while len(queue) > 0:
+			#print('h')
 			vertex = queue.pop(0)
 			if vertex not in visited:
 				visited.add(vertex)
+				#if len(visited) == degree and start not in graph[vertex]:
+				#	continue
 				tmp = copy.deepcopy(visited)
-				for item in tmp:
+				#print(tmp, vertex)
+				for item in visited:
 					if item not in graph[vertex] and item != vertex:
-						visited.remove(item)
-				if len(visited) == degree and start in graph[vertex]:
-					print ('in loop', visited)
+						tmp.remove(item)
+				if len(tmp) == degree and start in graph[vertex]:
+					print ('in loop', tmp)
 					#visitList = [e for e in visited]
-					tmp = copy.copy(visited)
+					#tmp = copy.copy(visited)
 					if tmp not in result:
-						result.append(tmp)
-					visited.remove(vertex)
+						tmp2 = copy.deepcopy(tmp)
+						result.append(tmp2)
+					tmp.remove(vertex)
 					continue
 				for v in graph[vertex]:
 					if v not in visited:
 						queue.append(v)
-				
-
 		return result
 
 soln = Solution()
 graph = soln.generateGraph('threeMusketeers.txt')
-print(soln.findCycle(graph, 1, 3))
+print(soln.findCycle(graph, 4, degree=3))
